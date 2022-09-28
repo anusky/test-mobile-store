63e74e7f3575f8561b066f65522ae5f67d17e8c5
Anusky <anusky.pitrusky@gmail.com>
2022-09-28 12:10:01 +0200

    develop - wip - add a little bit of documentation

--- src/utils/cartManagement.js
+++ src/utils/cartManagement.js
@@ -1,11 +1,25 @@
 import { LOCAL_STORAGE_TYPES, PAGE_STRUCTURE } from "./constants";
 
+/**
+ * @private
+ * @function setCart
+ * @param {object} data current cart
+ */
 const setCart = (data) =>
   window?.localStorage.setItem(LOCAL_STORAGE_TYPES.cart, JSON.stringify(data));
 
+/**
+ * @function getCart retrieve cart from localStorage
+ * @returns current cart list
+ */
 export const getCart = () =>
   JSON.parse(window?.localStorage.getItem(LOCAL_STORAGE_TYPES.cart)) || [];
 
+/**
+ * @function addProductToCart add a product in the cart and stores it in localStorage
+ * @param {object} productInfo
+ * @returns the product if everything worked or error otherwise
+ */
 export const addProductToCart = async (productInfo) => {
   return await fetch(PAGE_STRUCTURE.cart.clientApiLoader, {
     method: "POST",
--- src/utils/productManagement.js
+++ src/utils/productManagement.js
@@ -5,25 +5,56 @@ import {
   PAGE_STRUCTURE,
 } from "./constants";
 
+/**
+ * @private
+ * @function getDataExpirationDate get the expiration date from localStorage
+ * @returns expiration date from localStorage
+ */
 const getDataExpirationDate = () =>
   +window?.localStorage.getItem(LOCAL_STORAGE_TYPES.expiration);
 
+/**
+ * @private
+ * @function setDataExpirationDate sets the expiration date on localStorage
+ */
 const setDataExpirationDate = () =>
   window?.localStorage.setItem(
     LOCAL_STORAGE_TYPES.expiration,
     Date.now() + EXPIRATION_TIME_LIMIT
   );
+
+/**
+ * @private
+ * @function  etProductData saves in localStorage the current product information
+ * @param {object} data product information
+ */
 const setProductData = (data) =>
   window?.localStorage.setItem(
     LOCAL_STORAGE_TYPES.productData,
     JSON.stringify(data)
   );
+
+/**
+ * @private
+ * @function getProductData
+ * @returns product information from localStorage
+ */
 const getProductData = () =>
   JSON.parse(window?.localStorage.getItem(LOCAL_STORAGE_TYPES.productData));
 
+/**
+ * @private
+ * @function setDataOrigin sets if data was retrieved from api or localStorage
+ * @param {string} origin
+ */
 const setDataOrigin = (origin) =>
   window?.localStorage.setItem(LOCAL_STORAGE_TYPES.origin, origin);
 
+/**
+ * @function getAlProducts
+ * @description retrieves product list from api or from localStorage. It depends on if expiration time was reached or not.
+ * @returns list of products
+ */
 export const getAlProducts = async () => {
   const expires = getDataExpirationDate();
   if (!expires || expires < Date.now() - EXPIRATION_TIME_LIMIT) {
@@ -47,9 +78,21 @@ export const getAlProducts = async () => {
   }
 };
 
+/**
+ * @private
+ * @function getProductByIdUrl get the correct product api url
+ * @param {string} id
+ * @returns product url
+ */
 const getProductByIdUrl = (id) =>
   PAGE_STRUCTURE.product.clientApiLoader.replace(":id", id);
 
+/**
+ * @function getProductById
+ * @param {string} id
+ * @description retrieves product information from api using its ID
+ * @returns product info
+ */
 export const getProductById = async (id) => {
   return await fetch(getProductByIdUrl(id))
     .then((res) => res.json())
